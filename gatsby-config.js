d6dd54313889e61ce8070f06e2bf336d5d403c0b
markbello <mcbello51286@gmail.com>
2020-05-21 11:30:39 -0400

    update messaging in index and gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    title: 'Gatsby Default Starter with Jest, Enzyme, and ESLint',
+    description: 'Kick off your next, great Gatsby project with this default starter with Jest, Enzyme, and ESLint. This barebones starter ships with the main Gatsby configuration files you might need, with testing utilities installed.',
     author: '@gatsbyjs',
   },
   plugins: [
